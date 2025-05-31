21bfa9b123db8579b52923bdc2a634908777d5a4
SBola97 <sebassenk1997@gmail.com>
2025-05-31 21:47:38 +0200

    Built services section and improved its component

---
+++ src/data/services.ts
@@ -0,0 +1,32 @@
+export const services = [
+    {
+        title: "LAVADO INTERIOR-EXTERIOR COMPLETOS",
+        image: "/src/assets/service1.jpg",
+        alt: "Servicio 1",
+        href: "#"
+    },
+    {
+        title: "LAVADO INTEGRAL DE TAPICERÍA",
+        image: "/src/assets/service2.jpg",
+        alt: "Servicio 2",
+        href: "#"
+    },
+    {
+        title: "MECÁNICA RÁPIDA",
+        image: "/src/assets/service3.jpg",
+        alt: "Servicio 3",
+        href: "#"
+    },
+    {
+        title: "REPARACIÓN DE TAPICERÍA",
+        image: "/src/assets/service4.jpg",
+        alt: "Servicio 4",
+        href: "#"
+    },
+    {
+        title: "TINTADO DE LUNAS",
+        image: "/src/assets/service5.jpg",
+        alt: "Servicio 5",
+        href: "#"
+    }
+]; 
